869ee0a4efa97eac0b0ff1efef6c663f7c9f0746
robfuscator <38418563+robfuscator@users.noreply.github.com>
2022-05-10 15:08:42 +0200

    Initial commit

---
+++ src/lib.ts
@@ -0,0 +1,10 @@
+/*
+ * typescript-library-template
+ *
+ * Author: Robin Bürkli <robuba.jr@gmx.ch>
+ * License: MIT
+ *
+ * This is the entrypoint to the library.
+ */
+
+export const HELLO_CONSUMER: string = 'Hello consumer!';
