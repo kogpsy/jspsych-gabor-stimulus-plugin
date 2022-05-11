b6abf8c3c5212bbe8337c0126d3ef96a9069a88c
Robin Bürkli <robuba.jr@gmx.ch>
2022-05-11 16:29:09 +0200

    Add code doc

--- src/FixationCrossGen.ts
+++ src/FixationCrossGen.ts
@@ -1,10 +1,29 @@
+/**
+ * FixationCrossGen
+ *
+ * Generate fixation crosses as SVGElements.
+ *
+ * Author: Robin Bürkli <robuba.jr@gmx.ch>
+ * License: GPL-3.0
+ */
+
+/**
+ * Generate an SVG based fixation cross
+ *
+ * @param stimulusSize The size of the actual stimulus (the fixation cross will
+ * be displayed centered)
+ * @param fixationCrossSize The size of the fixation cross (height and width)
+ * @param fixationCrossWeight The fixation cross weight
+ * @param fixationCrossColor The fixation cross color
+ * @returns The generated fixation cross as an HTMLElement (SVG)
+ */
 export const generateFixationCross = (
   stimulusSize: number,
   fixationCrossSize: number,
   fixationCrossWeight: number,
   fixationCrossColor: string
 ): HTMLElement => {
-  // Create the stimulus element
+  // Create and setup the stimulus element
   const svg = document.createElement('svg');
   svg.setAttribute('height', `${stimulusSize}`);
   svg.setAttribute('width', `${stimulusSize}`);
@@ -13,7 +32,7 @@ export const generateFixationCross = (
   svg.style.top = '0';
   svg.style.zIndex = '2';
 
-  // Create the line objects
+  // Create the horizontal line object and set its attributes
   const horizontalLine = document.createElement('line');
   horizontalLine.setAttribute('stroke-width', `${fixationCrossWeight}`);
   horizontalLine.setAttribute('stroke', fixationCrossColor);
@@ -28,6 +47,7 @@ export const generateFixationCross = (
     `${stimulusSize / 2 + fixationCrossSize / 2}`
   );
 
+  // Create the vertical line object and set its attributes
   const verticalLine = document.createElement('line');
   verticalLine.setAttribute('stroke-width', `${fixationCrossWeight}`);
   verticalLine.setAttribute('stroke', fixationCrossColor);
@@ -42,8 +62,10 @@ export const generateFixationCross = (
     `${stimulusSize / 2 + fixationCrossSize / 2}`
   );
 
+  // Append lines to svg
   svg.appendChild(horizontalLine);
   svg.appendChild(verticalLine);
 
+  // Return generated svg
   return svg;
 };
