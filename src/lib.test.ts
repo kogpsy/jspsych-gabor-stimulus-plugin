a832ab7a080441a3e0a917410313b6f9381cd207
Robin Bürkli <robuba.jr@gmx.ch>
2022-12-07 14:39:08 +0100

    Upgrade tooling

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -7,8 +7,8 @@
  * This is the file that tests 'lib.ts'.
  */
 
-import {HELLO_CONSUMER} from './lib';
+/* import {HELLO_CONSUMER} from './lib';
 
 test('HELLO_CONSUMER contains "Hello consumer!"', () => {
   expect(HELLO_CONSUMER).toBe('Hello consumer!');
-});
+}); */
